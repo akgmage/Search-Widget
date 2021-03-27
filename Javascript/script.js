18c630f0ec45203f50d75b85130f3b691c8136ab
Abhisek Kumar Gupta <65363296+akgmage@users.noreply.github.com>
2021-03-27 20:17:11 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,7 @@
 const search = document.querySelector('.search');
+
 const btn = document.querySelector('.btn');
+
 const input = document.querySelector('.input');
 
 btn.addEventListener('click', () => {
